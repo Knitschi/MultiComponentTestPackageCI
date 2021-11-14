0c524c6cf4568d11ee57278610dbe249ad6a3c76
Knitschi <knitschi.public@freenet.de>
2021-11-14 12:05:37 +0100

    Correction of package list

--- Sources/packages.cmake
+++ Sources/packages.cmake
@@ -1,8 +1,6 @@
 set( CPF_PACKAGES
 	EXTERNAL CIBuildConfigurations
-    EXTERNAL cotire
 	EXTERNAL CPFCMake
     EXTERNAL CPFBuildscripts
-    OWNED MyLib
-    OWNED documentation
+    OWNED MultiComponentTestPackage
 )
