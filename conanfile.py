a3252d1f8fbf27ee0ee6ebe06eac3c7a9f856b9a
Knitschi <knitschi.public@freenet.de>
2022-02-11 13:08:59 +0100

    Updates packages and conanfile requirements.

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class MultiComponentTestPackage(ConanFile):
     description = "A package that is created by the MultiComponentTestPackage repository."
 
     # Dependencies
-    python_requires = "CPFConanfile/0.0.16@knitschi/development",
+    python_requires = "CPFConanfile/0.0.20@knitschi/development",
     python_requires_extend = "CPFConanfile.CPFBaseConanfile",
 
     cpf_conanfile_module = None
